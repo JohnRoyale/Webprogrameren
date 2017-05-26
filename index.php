d4b33d0d405b3923a908db0591afc8d53280ae13
Jasper Krebbers <jasperkrebbers@msn.com>
2017-05-26 10:05:48 +0200

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title>Multiplayer</title>
+  </head>
+  <body>
+    <h1>Tic tac toe</h1>
+    <p>Multiplayer mayhem</p>
+
+	<?php
+		
+	?>
+
+    <script src="main.js"></script>
+  </body>
+</html>
